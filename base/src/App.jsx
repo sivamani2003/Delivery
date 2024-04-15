1da3b7f3902ef8362fed8700f6ed27c1bbc626b4
siva <sivamani_k@srmap.edu.in>
2024-04-15 15:44:35 +0530

    first

---
+++ base/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './components/Navbar/Navbar'
+import "./App.css"
+const App = () => {
+  return (
+    <div className='app'> 
+      <Navbar></Navbar>
+    </div>
+  )
+}
+
+export default App
