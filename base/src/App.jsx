b018ef2e7d84718d0dc9309aa3d64c01a5f50d60
siva <sivamani_k@srmap.edu.in>
2024-04-16 10:26:33 +0530

    header part done

--- base/src/App.jsx
+++ base/src/App.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import "./App.css"
+import { Route, Routes } from 'react-router-dom'
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
+import Home from './pages/Home/Home'
+import Cart from './pages/Cart/Cart'
 const App = () => {
   return (
     <div className='app'> 
       <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/cart' element={<Cart></Cart>}></Route>
+        <Route path='/order' element={<PlaceOrder></PlaceOrder>}></Route>
+      </Routes>
     </div>
   )
 }
---
+++ base/src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Header.css"
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Order The Food</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia magni porro ea similique, voluptatum consequuntur minus et tempora sunt commodi officiis vitae quidem accusamus, deleniti est magnam, qui quos? Porro?</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
