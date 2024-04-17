676a4ba70ed585b60be4dede41ff75a30194d279
siva <sivamani_k@srmap.edu.in>
2024-04-17 22:21:58 +0530

    update

--- admin/src/pages/List/List.jsx
+++ admin/src/pages/List/List.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './List.css'
+import axios from 'axios'
+import { toast } from 'react-toastify';
 const List = () => {
+  const [list,setList]=useState([]);
+  const url = "http://localhost:4010";
+  const fetchList = async ()=>{
+    const response = await axios.get(`${url}/api/food/list`)
+    console.log(response.data)
+    if(response.data.success){
+      setList(response.data.data)
+    }
+    else{
+      toast.error("Error")
+    }
+  }
+  // useEffect(()=>{
+  //   fetchList();
+  // },)
   return (
-    <div>
-      list
+    <div className='list add flex-col'>
+      <p>All Foods List</p>
+      <div className="list-table">
+        <div className="list-table-format title">
+          <b>Image</b>
+          <b>Name</b>
+          <b>Category</b>
+          <b>Price</b>
+          <b>Action</b>
+        </div>
+        {list.map((item,index)=>{
+          return(
+            <div>
+                q
+            </div>
+          )
+        })
+          
+        }
+      </div>
     </div>
   )
 }
