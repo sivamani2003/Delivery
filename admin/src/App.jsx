8dc8befac021d32e5fe5959c91e7fee60e30607f
siva <sivamani_k@srmap.edu.in>
2024-04-17 22:08:11 +0530

    update

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
-
+import Navbar from './componets/Navbar/Navbar'
+import Sidebar from './componets/sidebar/Sidebar'
+import { Route, Routes } from 'react-router-dom'
+import Add from './pages/Add/Add'
+import List from './pages/List/List'
+import Orders from './pages/Orders/Orders'
+import { ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 const App = () => {
   return (
     <div>
-      hello
+      <ToastContainer/>
+      <Navbar>
+       
+      </Navbar>
+      <hr />
+      <div className="app-content">
+        <Sidebar/>
+          <Routes>
+            <Route path='/add' element={<Add/>}></Route>
+            <Route path='/list' element={<List/>}></Route>
+            <Route path='/order' element={<Orders/>}></Route>
+          </Routes>
+
+      </div>
     </div>
   )
 }
